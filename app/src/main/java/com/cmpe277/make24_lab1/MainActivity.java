9c814903934cb67cfbfa2405c83a8b0904d620f0
shiva322 <shivapadma322@gmail.com>
2018-03-09 20:16:01 -0800

    Initial commit

---
+++ app/src/main/java/com/cmpe277/make24_lab1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cmpe277.make24_lab1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
